07a9bccf4ffc5220eeee382cd170144c4cecba45
Patrick Lee <zpatricklee@gmail.com>
2026-04-09 22:10:47 -0700

    fixing header and footer issues

--- js/include.js
+++ js/include.js
@@ -1,4 +1,4 @@
-fetch('/partials/home-banner.html')
+fetch('./partials/home-banner.html')
   .then((response) => response.text())
   .then((html) => {
     document.getElementById('home-banner-container').innerHTML = html;
@@ -75,7 +75,7 @@ fetch('/partials/home-banner.html')
   })
   .catch((error) => console.error('Error loading home-banner.html:', error));
 
-fetch('/partials/footer.html')
+fetch('./partials/footer.html')
   .then((response) => response.text())
   .then((html) => {
     document.getElementById('footer-container').innerHTML = html;
